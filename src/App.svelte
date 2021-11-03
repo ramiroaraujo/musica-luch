9f0dce217033fff1e64f4371b0fb94cf84d536f1
Ramiro Araujo <rama.araujo@gmail.com>
2021-11-02 21:15:41 -0300

    base app

---
+++ src/App.svelte
@@ -0,0 +1,86 @@
+<script>
+    import Button from "./Button.svelte";
+
+    export let name;
+    let audios = [
+        {
+            name: 'Musica de fondo',
+            reset: false,
+            loop: true,
+            from: 0,
+            to: null,
+            src: 'bg.mp3',
+            color: '#dba7ef'
+        },
+        {
+            name: 'Payaso plin plin',
+            reset: true,
+            loop: false,
+            from: 0,
+            to: null,
+            src: 'payaso.mp3',
+            color: '#cd6f6f'
+        },
+        {
+            name: 'Disco rayado',
+            reset: true,
+            loop: false,
+            from: 0,
+            to: null,
+            src: 'disco.mp3',
+            color: '#90cb8a'
+        }
+    ]
+    let bg;
+</script>
+
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 title">
+                <h1>Música Luch</h1>
+            </div>
+            <Button bind:this={bg} audio={audios[0]}/>
+            {#each audios.slice(1) as audio}
+                <Button audio={audio} on:volumedown={()=>{bg.volumeDown()}} on:volumeup={()=>{bg.volumeUp()}}/>
+            {/each}
+        </div>
+        <div class="col-12 reload">
+            <button on:click={()=>{location.reload(true)}}>Recargar</button>
+        </div>
+    </div>
+</main>
+
+<style>
+    .title {
+        margin-bottom: 50px;
+        font-weight: bold;
+    }
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+    .reload {
+        margin-top:150px;
+    }
+    .reload button {
+        width: 200px;
+        height: 200px;
+        background-color: orange;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+</style>
---
+++ src/Button.svelte
@@ -0,0 +1,57 @@
+<svelte:options accessors/>
+<script>
+    import {createEventDispatcher} from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    export let audio;
+    export let volume = 1;
+    export let volumeDown = () => {
+        console.log('down');
+        paused = true
+    }
+    export let volumeUp = () => {
+        console.log('up');
+        volume = 1;
+    }
+    let paused = true;
+    let currentTime = 0;
+    let onClick = () => {
+        if (!paused) {
+            paused = true;
+            dispatch('volumeup')
+        } else {
+            paused = false;
+            if (audio.reset) {
+                dispatch('volumedown')
+                currentTime = 0;
+            }
+
+        }
+    }
+    let finished = () => {
+        paused = true;
+        currentTime = 0;
+        dispatch('volumeup')
+    }
+</script>
+<div class="col-4">
+    <button style='--color: {audio.color}' on:mouseup={onClick}>{audio.name}</button>
+    <audio sty src={audio.src} loop={audio.loop} bind:paused={paused} bind:currentTime={currentTime}
+           bind:volume={volume}
+           on:ended={finished}></audio>
+</div>
+<style>
+    div.col-4 {
+        display: flex;
+        justify-content: center;
+    }
+
+    button {
+        border-radius: 50%;
+        display: block;
+        width: 200px;
+        height: 200px;
+        background-color: var(--color);
+    }
+</style>
